7e325b10de91f7db6227e8cae1e2338df7546eff
kevansevans <kbeevans@gmail.com>
2017-03-03 16:06:17 -0700

    Track dragging
    Moved track dragging from pencil to tool base so all tools have this function, unless otherwise overwritten in class.

---
+++ src/ui/tool/tool/lr/Pencil.hx
@@ -0,0 +1,42 @@
+package ui.tool.lr;
+
+import lr.line.LineBase;
+import openfl.events.MouseEvent;
+import openfl.geom.Point;
+
+import global.Common;
+import ui.tool.ToolBase;
+
+/**
+ * ...
+ * @author Kaelan Evans
+ */
+class Pencil extends ToolBase
+{
+	private var a:Point;
+	private var b:Point;
+	public function new() 
+	{
+		super();
+	}
+	override public function mouseDown(e:MouseEvent) {
+		a = new Point(Common.gTrack.mouseX, Common.gTrack.mouseY);
+		trace(a);
+		Common.gStage.addEventListener(MouseEvent.MOUSE_MOVE, pencil_move);
+	}
+	
+	private function pencil_move(e:MouseEvent):Void 
+	{
+		b = new Point(Common.gTrack.mouseX, Common.gTrack.mouseY);
+		Common.gTrack.render_preview_line(a, b);
+		if (Common.get_distance(a, b) >= Common.line_minLength) {
+			var _loc1 = new LineBase(a, b);
+			Common.gTrack.add_vis_line(_loc1);
+			a = new Point(Common.gTrack.mouseX, Common.gTrack.mouseY);
+		}
+	}
+	override public function mouseUp(e:MouseEvent) {
+		Common.gStage.removeEventListener(MouseEvent.MOUSE_MOVE, pencil_move);
+		Common.gTrack.clear_preview();
+	}
+}
