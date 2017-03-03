fb802eedbd87b201724301c8a2029c3f92a8d672
kevansevans <kbeevans@gmail.com>
2017-03-03 12:46:01 -0700

    Tool Base
    Added event listeners for left, right, and middle. Can detect press and release.

--- src/ui/tool/ToolBase.hx
+++ src/ui/tool/ToolBase.hx
@@ -14,11 +14,36 @@ import global.Common;
 class ToolBase
 {
 	public var type:String = "Null";
+	
 
 	public function new() 
 	{
 		Common.gStage.addEventListener(MouseEvent.MOUSE_DOWN, mouseDown);
 		Common.gStage.addEventListener(MouseEvent.MOUSE_UP, mouseUp);
+		Common.gStage.addEventListener(MouseEvent.RIGHT_MOUSE_DOWN, rMouseDown);
+		Common.gStage.addEventListener(MouseEvent.RIGHT_MOUSE_UP, rMouseUp);
+		Common.gStage.addEventListener(MouseEvent.MIDDLE_MOUSE_DOWN, mMouseDown);
+		Common.gStage.addEventListener(MouseEvent.MIDDLE_MOUSE_UP, mMouseUp);
+	}
+	
+	public function mMouseUp(e:MouseEvent):Void 
+	{
+		trace(Common.gStage.mouseX, Common.gStage.mouseY);
+	}
+	
+	public function mMouseDown(e:MouseEvent):Void 
+	{
+		trace(Common.gStage.mouseX, Common.gStage.mouseY);
+	}
+	
+	public function rMouseUp(e:MouseEvent):Void 
+	{
+		trace(Common.gStage.mouseX, Common.gStage.mouseY);
+	}
+	
+	public function rMouseDown(e:MouseEvent):Void 
+	{
+		trace(Common.gStage.mouseX, Common.gStage.mouseY);
 	}
 	
 	public function mouseUp(e:MouseEvent):Void 
