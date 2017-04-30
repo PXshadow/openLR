15caa8114356f74e473704a5c5f8d28fae169468
kevansevans <kbeevans@gmail.com>
2017-04-30 16:21:02 -0600

    Keybindsings tweaks

--- src/global/KeyBindings.hx
+++ src/global/KeyBindings.hx
@@ -37,6 +37,8 @@ class KeyBindings
 	public static var sm_toggle:Int = Keyboard.M;
 	public static var rw_toggle:Int = Keyboard.B; //sim only
 	public static var pp_toggle:Int = Keyboard.SPACE;
+	public static var step_forward:Int = Keyboard.RIGHT;
+	public static var step_backward:Int = Keyboard.LEFT;
 	public static var undo_line:Int = Keyboard.BACKSPACE;
 	
 	//combo strokes
@@ -48,41 +50,6 @@ class KeyBindings
 	{
 		KeyBindings.KeyStringList = new Array();
 		this.setArrayBinds();
-	}
-	public static function get_json_defaults():Object {
-		var key:Object = new Object();
-		key = {
-			"defaults" : false,
-			"binds" : {
-				"pencil_1" : Keyboard.Q,
-				"pencil_2" : Keyboard.F1,
-				"line_1" : Keyboard.W,
-				"line_2" : Keyboard.F2,
-				"eraser_1" : Keyboard.E,
-				"eraser_2" : Keyboard.F3,
-				"pan_1" : Keyboard.R,
-				"pan_2" : Keyboard.F4,
-				"swatch_blue" : Keyboard.NUMBER_1,
-				"swatch_red" : Keyboard.NUMBER_2,
-				"swatch_green" : Keyboard.NUMBER_3,
-				"icon_play" : Keyboard.Y,
-				"icon_stop" : Keyboard.U,
-				"icon_flag" : Keyboard.I,
-				"mod_action_shift" : Keyboard.SHIFT,
-				"mod_action_control" : Keyboard.CONTROL,
-				"angle_snap" : Keyboard.X,
-				"line_snap" : Keyboard.S,
-				"ff_toggle" : Keyboard.N,
-				"sm_toggle" : Keyboard.M,
-				"rw_toggle" : Keyboard.B,
-				"pp_toggle" : Keyboard.SPACE,
-				"undo_stroke" : Keyboard.Z,
-				"redo_stroke" : Keyboard.Y,
-				"redo_line" : Keyboard.BACKSPACE
-			},
-			"reference" : "http://api.openfl.org/openfl/ui/Keyboard.html"
-		}
-		return(key);
 	}
 	public static function reset() {
 		KeyBindings.pencil_1 = Keyboard.Q;
@@ -107,13 +74,53 @@ class KeyBindings
 		KeyBindings.sm_toggle = Keyboard.M;
 		KeyBindings.rw_toggle = Keyboard.B;
 		KeyBindings.pp_toggle = Keyboard.SPACE;
+		KeyBindings.step_forward = Keyboard.RIGHT;
+		KeyBindings.step_backward = Keyboard.LEFT;
 		KeyBindings.undo_line = Keyboard.BACKSPACE;
 		
 		KeyBindings.undo_stroke = Keyboard.Z;
 		KeyBindings.redo_stroke = Keyboard.Y;
 		KeyBindings.redo_line = Keyboard.BACKSPACE;
 	}
-	
+	static public function write_settings() {
+		var key:Object = new Object();
+		key = {
+			"defaults" : false,
+			"binds" : {
+				"pencil_1" : KeyBindings.pencil_1,
+				"pencil_2" : KeyBindings.pencil_2,
+				"line_1" : KeyBindings.line_1,
+				"line_2" : KeyBindings.line_2,
+				"eraser_1" : KeyBindings.eraser_1,
+				"eraser_2" : KeyBindings.eraser_2,
+				"pan_1" : KeyBindings.pan_1,
+				"pan_2" : KeyBindings.pan_2,
+				"swatch_blue" : KeyBindings.swatch_blue,
+				"swatch_red" : KeyBindings.swatch_red,
+				"swatch_green" : KeyBindings.swatch_green,
+				"icon_play" : KeyBindings.icon_play,
+				"icon_stop" : KeyBindings.icon_stop,
+				"icon_flag" : KeyBindings.icon_flag,
+				"mod_action_shift" : KeyBindings.mod_action_shift,
+				"mod_action_control" : KeyBindings.mod_action_control,
+				"angle_snap" : KeyBindings.angle_snap,
+				"line_snap" : KeyBindings.line_snap,
+				"ff_toggle" : KeyBindings.ff_toggle,
+				"sm_toggle" : KeyBindings.sm_toggle,
+				"rw_toggle" : KeyBindings.rw_toggle,
+				"pp_toggle" : KeyBindings.pp_toggle,
+				"step_forward" : KeyBindings.step_forward.
+				"step_backward" : KeyBindings.step_backward,
+				"undo_stroke" : KeyBindings.undo_stroke,
+				"redo_stroke" : KeyBindings.redo_stroke,
+				"redo_line" : KeyBindings.redo_line
+			},
+			"reference" : "http://api.openfl.org/openfl/ui/Keyboard.html"
+		}
+		var file = File.write("./settings/KeyBindings.json", true);
+		file.writeString(Json.stringify(key, null, "\t"));
+		file.close();
+	}
 	static public function set_bindings(object) 
 	{
 		if (object.defaults == true) {
@@ -143,6 +150,8 @@ class KeyBindings
 		KeyBindings.sm_toggle = object.binds.sm_toggle;
 		KeyBindings.rw_toggle = object.binds.rw_toggle;
 		KeyBindings.pp_toggle = object.binds.pp_toggle;
+		KeyBindings.step_forward = object.binds.step_forward;
+		KeyBindings.step_backward = object.binds.step_backward;
 		KeyBindings.undo_line = object.binds.undo_line;
 		KeyBindings.check_null();
 	}
@@ -227,6 +236,12 @@ class KeyBindings
 		if (KeyBindings.redo_line == 0) {
 			KeyBindings.redo_line = Keyboard.BACKSPACE;
 		}
+		if (KeyBindings.step_forward == 0) {
+			KeyBindings.step_forward = Keyboard.RIGHT;
+		}
+		if (KeyBindings.step_backward == 0) {
+			KeyBindings.step_backward = Keyboard.RIGHT;
+		}
 		KeyBindings.write_settings();
 	}
 	private function setArrayBinds() 
@@ -286,42 +301,7 @@ class KeyBindings
 		KeyBindings.KeyStringList[Keyboard.SHIFT] = "Shift"; //Might not want to have shift and control rebindable for everything that uses it
 		KeyBindings.KeyStringList[Keyboard.CONTROL] = "Ctrl";
 		KeyBindings.KeyStringList[Keyboard.SPACE] = "Space";
-	}
-	static public function write_settings() {
-		var key:Object = new Object();
-		key = {
-			"defaults" : false,
-			"binds" : {
-				"pencil_1" : KeyBindings.pencil_1,
-				"pencil_2" : KeyBindings.pencil_2,
-				"line_1" : KeyBindings.line_1,
-				"line_2" : KeyBindings.line_2,
-				"eraser_1" : KeyBindings.eraser_1,
-				"eraser_2" : KeyBindings.eraser_2,
-				"pan_1" : KeyBindings.pan_1,
-				"pan_2" : KeyBindings.pan_2,
-				"swatch_blue" : KeyBindings.swatch_blue,
-				"swatch_red" : KeyBindings.swatch_red,
-				"swatch_green" : KeyBindings.swatch_green,
-				"icon_play" : KeyBindings.icon_play,
-				"icon_stop" : KeyBindings.icon_stop,
-				"icon_flag" : KeyBindings.icon_flag,
-				"mod_action_shift" : KeyBindings.mod_action_shift,
-				"mod_action_control" : KeyBindings.mod_action_control,
-				"angle_snap" : KeyBindings.angle_snap,
-				"line_snap" : KeyBindings.line_snap,
-				"ff_toggle" : KeyBindings.ff_toggle,
-				"sm_toggle" : KeyBindings.sm_toggle,
-				"rw_toggle" : KeyBindings.rw_toggle,
-				"pp_toggle" : KeyBindings.pp_toggle,
-				"undo_stroke" : KeyBindings.undo_stroke,
-				"redo_stroke" : KeyBindings.redo_stroke,
-				"redo_line" : KeyBindings.redo_line
-			},
-			"reference" : "http://api.openfl.org/openfl/ui/Keyboard.html"
-		}
-		var file = File.write("./settings/KeyBindings.json", true);
-		file.writeString(Json.stringify(key, null, "\t"));
-		file.close();
+		KeyBindings.KeyStringList[Keyboard.LEFT] = "Left";
+		KeyBindings.KeyStringList[Keyboard.RIGHT] = "Right";
 	}
 }
