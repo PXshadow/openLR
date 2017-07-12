bd567470adf924a357f46cbd598ae265662bb78a
kevansevans <kbeevans@gmail.com>
2017-07-12 16:17:15 -0600

    basic class changed to inline

--- src/file/ui/FileButton.hx
+++ src/file/ui/FileButton.hx
@@ -10,7 +10,7 @@ class FileButton extends SimpleButton
 	public var ID:Int = 0;
 	public var filePath:String;
 	public var file_parent:FileItem;
-	public function new(_id:Int, _path:String, _parent:FileItem) 
+	public inline function new(_id:Int, _path:String, _parent:FileItem) 
 	{
 		super();
 		this.ID = _id;
