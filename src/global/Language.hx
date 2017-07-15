f9d3323fc68146c1ad0c001ff2d27993f2a093b4
kevansevans <kbeevans@gmail.com>
2017-07-15 16:08:23 -0600

    Okay

--- src/global/Language.hx
+++ src/global/Language.hx
@@ -14,9 +14,9 @@ class Language
 	public static var Welcome:String;
 	public static var Continue:String;
 	public static var Yes:String;
+	public static var Okay:String;
 	public static var No:String;
 	public static var Cancel:String;
 	public static var Load:String;
 	public static var Save:String;
-	public static var Okay:String;
 }
