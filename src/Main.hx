47d86523c370a452a8648b19d85954c06cf5f404
kevansevans <kbeevans@gmail.com>
2017-07-15 17:12:38 -0600

    Fixed starting enviorment
    Tools were still accessible in the splash screen, causing unwanted behavior when switching tools.

--- src/Main.hx
+++ src/Main.hx
@@ -102,9 +102,6 @@ class Main extends Sprite
 		
 		this.mainFileInit = new FileStart(); //checks for default folders and saved settings in the track
 		
-		this.init_env();
-		this.init_track();
-		
 		this.welcome_alert = new AlertBox(Language.Welcome + "\n" + "\n" + "Version: " + Common.version, this.start, Language.Continue);
 		this.addChild(this.welcome_alert);
 		this.welcome_alert.x = (this.stage.stageWidth * 0.5) - (this.welcome_alert.width * 0.5);
@@ -113,18 +110,20 @@ class Main extends Sprite
 		//trace(HXLZString.compress("Hello, my name is Kevansevans"));
 	}
 	function start() {
+		this.init_env();
+		this.init_track();
 		this.removeChild(this.welcome_alert);
 		this.visContainer.visible = true;
 	}
 	public function init_env() //Initialize enviornment
 	{
+		this.stage.addEventListener(Event.RESIZE, resize);
+		
 		Common.gCode = this; //This class
 		Common.gStage = this.stage; //The stage, not to be comfused with main.hx
 		
 		Common.stage_height = this.stage.stageHeight;
 		Common.stage_width = this.stage.stageWidth;
-		
-		this.stage.addEventListener(Event.RESIZE, resize);
 		
 		this.FPS = new FrameRate();
 		
@@ -172,6 +171,8 @@ class Main extends Sprite
 		this.timeline.update();
 		
 		this.visContainer.visible = false;
+		
+		this.align();
 	}
 	public function reset_timeline() {
 		Common.sim_frames = 0;
@@ -248,6 +249,32 @@ class Main extends Sprite
 	}
 	private function resize(e:Event):Void
 	{
+		this.visContainer.x = this.visContainer.y = 0;
+		this.toolBar.x = (this.stage.stageWidth * 0.5) - (this.toolBar.width * 0.25); 
+		
+		Common.stage_height = this.stage.stageHeight;
+		Common.stage_width = this.stage.stageWidth;
+		
+		this.textInfo.x = (this.stage.stageWidth - this.textInfo.width) - 5;
+		this.textInfo.y = 5;
+		
+		if (Common.gCamera != null) {
+			Common.gCamera.update_pan_bounds();
+		}
+		
+		this.save_manager.x = (this.stage.stageWidth * 0.5) - (this.save_manager.width * 0.5);
+		this.save_manager.y = (this.stage.stageHeight * 0.5) - (this.save_manager.height * 0.5);
+		
+		this.settings_box.x = (this.stage.stageWidth * 0.5) - (this.settings_box.width * 0.5);
+		this.settings_box.y = (this.stage.stageHeight * 0.5) - (this.settings_box.height * 0.5);
+		
+		this.loadManager.x = (this.stage.stageWidth * 0.5) - (this.loadManager.width * 0.5);
+		this.loadManager.y = (this.stage.stageHeight * 0.5) - 300;
+		
+		this.timeline.x = (this.stage.stageWidth * 0.5) - (this.timeline.width * 0.5);
+		this.timeline.y = this.stage.stageHeight - this.timeline.height + 25;
+	}
+	public function align() {
 		this.visContainer.x = this.visContainer.y = 0;
 		this.toolBar.x = (this.stage.stageWidth * 0.5) - (this.toolBar.width * 0.25); 
 		
