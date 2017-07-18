6bc2325ab2b5bb09ac11c40c21fd91610a6c0896
kevansevans <kbeevans@gmail.com>
2017-07-17 22:16:56 -0600

    Reduce file count

--- src/ui/inter/TextButton.hx
+++ src/ui/inter/TextButton.hx
@@ -7,8 +7,7 @@ import openfl.text.TextFormatAlign;
 import openfl.utils.Assets;
 import openfl.display.Bitmap;
 import openfl.display.BitmapData;
-
-import ui.inter.BTNVisual;
+import openfl.display.SimpleButton;
 
 /**
  * ...
@@ -54,4 +53,27 @@ class TextButton extends Sprite
 				this.label.y = 5;
 		}
 	}
+}
+
+class BTNVisual extends SimpleButton
+{
+
+	public inline function new(_size) 
+	{
+		super();
+		switch(_size) {
+			default:
+			case 0:
+				this.upState = new Bitmap(Assets.getBitmapData("img/ui/230x40up.png"));
+				this.overState = new Bitmap(Assets.getBitmapData("img/ui/230x40over.png"));
+				this.downState = new Bitmap(Assets.getBitmapData("img/ui/230x40down.png"));
+				this.hitTestState = new Bitmap(Assets.getBitmapData("img/ui/230x40up.png"));
+			case 1:
+				this.upState = new Bitmap(Assets.getBitmapData("img/ui/120x30up.png"));
+				this.overState = new Bitmap(Assets.getBitmapData("img/ui/120x30over.png"));
+				this.downState = new Bitmap(Assets.getBitmapData("img/ui/120x30down.png"));
+				this.hitTestState = new Bitmap(Assets.getBitmapData("img/ui/120x30up.png"));
+		}
+	}
+	
 }
