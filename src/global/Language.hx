be4d10c3aa275f2fea6f758f8962240aae4d4e24
kevansevans <kbeevans@gmail.com>
2017-07-17 21:12:38 -0600

    Global language vars

--- src/global/Language.hx
+++ src/global/Language.hx
@@ -11,7 +11,9 @@ class Language
 	public function new() {
 		
 	}
-	public static var Welcome:String;
+	public static var Title:String;
+	public static var Splash_a:String;
+	public static var Splash_b:String;
 	public static var Continue:String;
 	public static var Yes:String;
 	public static var Okay:String;
@@ -19,4 +21,6 @@ class Language
 	public static var Cancel:String;
 	public static var Load:String;
 	public static var Save:String;
+	public static var New_track:String;
+	public static var Load_track:String;
 }
