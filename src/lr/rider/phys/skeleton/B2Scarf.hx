063d73caa73d09bbf30426ec380b9878ed51014f
kevansevans <kbeevans@gmail.com>
2017-07-17 23:00:47 -0600

    Prepwork for class organization and cleanup

---
+++ src/lr/rider/phys/skeleton/B2Scarf.hx
@@ -0,0 +1,17 @@
+package lr.rider.phys.skeleton;
+import haxe.ds.Vector;
+import lr.rider.phys.anchors.SPoint;
+
+/**
+ * ...
+ * @author Kaelan Evans
+ */
+class B2Scarf 
+{
+	private var anchors:Vector<SPoint>;
+	public function new(_anchors:Vector<SPoint>) 
+	{
+		this.anchors = _anchors;
+	}
+	
+}
---
+++ src/lr/rider/phys/skeleton/B2Skeleton.hx
@@ -0,0 +1,17 @@
+package lr.rider.phys.skeleton;
+
+import lr.rider.phys.anchors.CPoint;
+
+/**
+ * ...
+ * @author Kaelan Evans
+ */
+class B2Skeleton 
+{
+	private var anchors:Array<CPoint>;
+	public function new(_anchors:Array<CPoint>)
+	{
+		this.anchors = _anchors;
+	}
+	
+}
