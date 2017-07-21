f394c2b8641ebf75e38e5cf363b509cf09606d90
kevansevans <kbeevans@gmail.com>
2017-07-21 16:39:41 -0600

    added if statement for preview
    that for some reason is believed to be a massive edit of the whole case switch

--- src/lr/scene/Track.hx
+++ src/lr/scene/Track.hx
@@ -55,26 +55,28 @@ class Track extends Sprite
 	public function renderPreview(_line:LineBase)
 	{
 		this.graphics.clear();
-		var _loc_3:Float = _line.nx > 0 ? (Math.ceil(_line.nx)) : (Math.floor(_line.nx));
-		var _loc_4:Float = _line.ny > 0 ? (Math.ceil(_line.ny)) : (Math.floor(_line.ny));
-		switch(_line.type) {
-			case LineType.Floor: 
-				this.graphics.lineStyle(2, 0x0066FF, 1, true, "normal", "round");
-				this.graphics.moveTo(_line.x1 + _loc_3, _line.y1 + _loc_4);
-				this.graphics.lineTo(_line.x2 + _loc_3, _line.y2 + _loc_4);
-			case LineType.Accel: 
-				this.graphics.lineStyle(2, 0xCC0000, 1, true, "normal", "round");
-				this.graphics.beginFill(0xCC0000, 1);
-				this.graphics.moveTo(_line.x1 + _loc_3, _line.y1 + _loc_4);
-				this.graphics.lineTo(_line.x2 + _loc_3, _line.y2 + _loc_4);
-				this.graphics.lineTo(_line.x2 + (_line.nx * 5 - _line.dx * _line.invDst * 5), _line.y2 + (_line.ny * 5 - _line.dy * _line.invDst * 5));
-				this.graphics.lineTo(_line.x2 - _line.dx * _line.invDst * 5, _line.y2 - _line.dy * _line.invDst * 5);
-				this.graphics.endFill();
-			case LineType.Scene: 
-				this.graphics.lineStyle(2, 0x00CC00, 1);
-				this.graphics.moveTo(_line.x1, _line.y1);
-				this.graphics.lineTo(_line.x2, _line.y2);
-				return;
+		if (!Common.cvar_preview_mode) {
+			var _loc_3:Float = _line.nx > 0 ? (Math.ceil(_line.nx)) : (Math.floor(_line.nx));
+			var _loc_4:Float = _line.ny > 0 ? (Math.ceil(_line.ny)) : (Math.floor(_line.ny));
+			switch(_line.type) {
+				case LineType.Floor: 
+					this.graphics.lineStyle(2, 0x0066FF, 1, true, "normal", "round");
+					this.graphics.moveTo(_line.x1 + _loc_3, _line.y1 + _loc_4);
+					this.graphics.lineTo(_line.x2 + _loc_3, _line.y2 + _loc_4);
+				case LineType.Accel: 
+					this.graphics.lineStyle(2, 0xCC0000, 1, true, "normal", "round");
+					this.graphics.beginFill(0xCC0000, 1);
+					this.graphics.moveTo(_line.x1 + _loc_3, _line.y1 + _loc_4);
+					this.graphics.lineTo(_line.x2 + _loc_3, _line.y2 + _loc_4);
+					this.graphics.lineTo(_line.x2 + (_line.nx * 5 - _line.dx * _line.invDst * 5), _line.y2 + (_line.ny * 5 - _line.dy * _line.invDst * 5));
+					this.graphics.lineTo(_line.x2 - _line.dx * _line.invDst * 5, _line.y2 - _line.dy * _line.invDst * 5);
+					this.graphics.endFill();
+				case LineType.Scene: 
+					this.graphics.lineStyle(2, 0x00CC00, 1);
+					this.graphics.moveTo(_line.x1, _line.y1);
+					this.graphics.lineTo(_line.x2, _line.y2);
+					return;
+			}
 		}
 		this.graphics.lineStyle(2, 0x000000, 1);
 		this.graphics.moveTo(_line.x1, _line.y1);
