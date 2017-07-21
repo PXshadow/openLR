00068bfa043c1cd446a1a1ec16bd39d5b3ab8172
kevansevans <kbeevans@gmail.com>
2017-07-20 22:35:21 -0600

    Change of inline function location

--- src/lr/lines/LineBase.hx
+++ src/lr/lines/LineBase.hx
@@ -180,3 +180,18 @@ class LineBase extends Shape
 		}
 	}
 }
+class LinePreview extends LineBase
+{
+
+	public inline function new(_x1:Float, _y1:Float, _x2:Float, _y2:Float, _inv:Bool, _type:Int) 
+	{
+		super();
+		this.type = _type;
+		x1 = _x1;
+		y1 = _y1;
+		x2 = _x2;
+		y2 = _y2;
+		inv = _inv;
+		this.calculateConstants();
+	}
+}
