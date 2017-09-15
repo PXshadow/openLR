288e1f5853d9b70adf826c2651cd7dfe68da8345
kevansevans <kbeevans@gmail.com>
2017-09-15 11:26:10 -0600

    Giving static vars defaults
    This should prevent crashing on other platforms that can't write a .json file immediately

--- src/global/Language.hx
+++ src/global/Language.hx
@@ -11,22 +11,22 @@ class Language
 	public function new() {
 		
 	}
-	public static var Title:String;
-	public static var Splash_a:String;
-	public static var Splash_b:String;
+	public static var Title:String = "Build by Kevansevans, original concept by Boštjan Čadež";
+	public static var Splash_a:String = "Welcome to OpenLR! The open source implementation of Line Rider for desktop use! You can view more info about the project here: ";
+	public static var Splash_b:String = "OpenLR uses the Haxe and OpenFL framework. View more info at Haxe.org and Openfl.org. Happy Riding!";
 	public static var Continue:String;
-	public static var Yes:String;
-	public static var Okay:String;
-	public static var No:String;
-	public static var Cancel:String;
-	public static var Load:String;
-	public static var Save:String;
-	public static var New_track:String;
-	public static var Load_track:String;
-	public static var Save_track:String;
-	public static var Screencap:String;
-	public static var File:String;
-	public static var Track:String;
-	public static var Profile:String;
-	public static var Close:String;
+	public static var Yes:String = "Yes";
+	public static var Okay:String = "Okay";
+	public static var No:String = "No";
+	public static var Cancel:String = "Cancel";
+	public static var Load:String = "Load";
+	public static var Save:String = "Save";
+	public static var New_track:String = "New Track";
+	public static var Load_track:String = "Load Track";
+	public static var Save_track:String = "Save Track";
+	public static var Screencap:String = "Screencap";
+	public static var File:String = "File";
+	public static var Track:String = "Track";
+	public static var Profile:String = "Profile";
+	public static var Close:String = "Close";
 }
