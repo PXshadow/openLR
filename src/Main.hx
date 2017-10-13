a86e4c3c8a669dc4daa5fb143a1250a248b0be14
Kaelan <kbeevans@gmail.com>
2017-10-12 20:58:55 -0600

    Android target planned for future

--- src/Main.hx
+++ src/Main.hx
@@ -3,17 +3,17 @@ package;
 import openfl.display.Sprite;
 
 import base.CoreBase;
-#if (cpp)
-	#if (!android)
-		import base.cores.CppCore;
-	#else
-		import base.cores.AndroidCore;
-	#end
-#elseif (flash)
-	import base.cores.FlashCore;
-#elseif (js)
-	import base.cores.JavaScriptCore;
+
+#if android
+    import base.cores.AndroidCore;        
+#elseif (windows || linux || osx)
+    import base.cores.CppCore;
+#elseif flash
+    import base.cores.FlashCore;
+#elseif js
+    import base.cores.JavaScriptCore;
 #end
+
 import global.Common;
 
 /**
@@ -30,23 +30,21 @@ import global.Common;
 
 class Main extends Sprite 
 {
-	private var core:CoreBase;
-	public function new() {
-		
-		super();
-		
-		#if (cpp)
-			#if (!android)
-				this.core = new CppCore(this.stage);
-			#else
-				this.core = new AndroidCore(this.stage);
-			#end
-		#elseif (flash)
-			this.core = new FlashCore(this.stage);
-		#elseif (js)
-			this.core = new JavaScriptCore(this.stage);
-		#else
-			trace("Deployment target not supported");
-		#end
-	}
+    private var core:CoreBase;
+    public function new() {
+        
+        super();
+        
+        #if android
+            this.core = new AndroidCore(this.stage);
+        #elseif (windows || linux || osx)
+            this.core = new CppCore(this.stage);
+        #elseif flash
+            this.core = new FlashCore(this.stage);
+        #elseif js
+            this.core = new JavaScriptCore(this.stage);
+        #else
+            trace("Deployment target not supported");
+        #end
+    }
 }
