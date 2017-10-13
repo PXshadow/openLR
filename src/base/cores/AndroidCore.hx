1b8a54d0025ec7e860f5aa69b99d027e1e80f231
Kaelan <kbeevans@gmail.com>
2017-10-12 18:53:52 -0600

    Forgot android core

---
+++ src/base/cores/AndroidCore.hx
@@ -0,0 +1,20 @@
+package base.cores;
+
+import openfl.display.Stage;
+
+import base.CoreBase;
+
+/**
+ * ...
+ * @author Kaelan Evans
+ */
+class AndroidCore extends CoreBase 
+{
+
+	public function new(_stage:Stage) 
+	{
+		super(_stage);
+		
+	}
+	
+}
