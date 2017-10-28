e1b564d30077bd31c28dea64cf3c0a4fc93082d9
Kaelan <kbeevans@gmail.com>
2017-10-27 22:50:29 -0600

    Conditional for toolbar assets on FL target

--- src/ui/tool/IconButton.hx
+++ src/ui/tool/IconButton.hx
@@ -1,8 +1,7 @@
 package ui.tool;
 
-import openfl.utils.AssetLibrary;
-
 #if (!flash)
+	import openfl.utils.AssetLibrary;
 	import openfl.display.SimpleButton;
 	import openfl.display.Sprite;
 #else
@@ -10,7 +9,6 @@ import openfl.utils.AssetLibrary;
 	import flash.display.SimpleButton;
 	import flash.display.Sprite;
 	import flash.display.MovieClip;
-	import platform.fl.ClipGrabber;
 #end
 	
 import ui.tool.IconBase;
@@ -34,26 +32,41 @@ class IconButton extends SimpleButton
 			this.attachFlashClips(_icon);
 		#end
 	}
-	function iconFailed(lib:AssetLibrary) 
-	{
-		trace("failed Icon");
-	}
-	
-	function attachClips(lib:AssetLibrary) 
-	{
-		this.upState = lib.getMovieClip("up");
-		this.hitTestState = lib.getMovieClip("up");
-		this.overState = lib.getMovieClip("over");
-		this.downState = lib.getMovieClip("down");
-	}
-	#if (flash)
-	function attachFlashClips(object:String) 
-	{
-		var _locClips = new ClipGrabber(object);
-		this.upState = _locClips.clipArray[0];
-		this.hitTestState = _locClips.clipArray[0];
-		this.overState = _locClips.clipArray[1];
-		this.downState = _locClips.clipArray[2];
-	}
+	#if (!flash)
+		function iconFailed(lib:AssetLibrary) 
+		{
+			trace("failed Icon");
+		}
+		function attachClips(lib:AssetLibrary) 
+		{
+			this.upState = lib.getMovieClip("up");
+			this.hitTestState = lib.getMovieClip("up");
+			this.overState = lib.getMovieClip("over");
+			this.downState = lib.getMovieClip("down");
+		}
+	#elseif (flash)
+		function attachFlashClips(_object:String) 
+		{
+			if (_object != Icon.swBlue && _object != Icon.swGreen && _object != Icon.swRed) {
+				this.upState = Assets.getMovieClip ("swf-library:up_" + _object);
+				this.hitTestState = Assets.getMovieClip ("swf-library:up_" + _object);
+				this.overState = Assets.getMovieClip ("swf-library:over_" + _object);
+				this.downState = Assets.getMovieClip ("swf-library:down_" + _object);
+			} else {
+				var nameSpace:String = "";
+				switch(_object) {
+					case Icon.swBlue :
+						nameSpace = "blue";
+					case Icon.swRed :
+						nameSpace = "red";
+					case Icon.swGreen :
+						nameSpace = "green";
+				}
+				this.upState = Assets.getMovieClip ("swf-library:up_" + nameSpace);
+				this.hitTestState = Assets.getMovieClip ("swf-library:up_" + nameSpace);
+				this.overState = Assets.getMovieClip ("swf-library:over_" + nameSpace);
+				this.downState = Assets.getMovieClip ("swf-library:down_" + nameSpace);
+			}
+		}
 	#end
 }
