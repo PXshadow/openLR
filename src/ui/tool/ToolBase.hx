d0f6560c300c0887c6770c186c315af2d3f40927
Kaelan <kbeevans@gmail.com>
2017-11-03 00:06:07 -0600

    Improved eraser function, prepwork for future tools
    Moved the alghorithm that checks location and moved it into ToolBase. This removes the need to constantly copy and paste a decent location function, and removes the exclusivity to the eraser

--- src/ui/tool/ToolBase.hx
+++ src/ui/tool/ToolBase.hx
@@ -6,8 +6,11 @@ import openfl.events.MouseEvent;
 import openfl.geom.Point;
 import openfl.events.KeyboardEvent;
 import openfl.ui.Keyboard;
+import openfl.utils.Object;
 import openfl.text.TextField;
 
+import lr.lines.LineBase;
+import lr.nodes.Grid;
 import global.KeyBindings;
 import global.Common;
 import global.CVar;
@@ -194,5 +197,45 @@ class ToolBase
 		var _locReturn:Array<Float> = new Array();
 		_locReturn = [x, y];
 		return(_locReturn);
+	}
+	public function locateLine(e:MouseEvent):LineBase {
+		var x:Float = Common.gTrack.mouseX;
+		var y:Float = Common.gTrack.mouseY;
+		var _loc20:Object = Common.gridPos(x, y);
+		var _loc9:Float = 1 / (Common.gTrack.scaleX);
+		for (_loc19 in -1...2)
+		{
+			var _loc7:Int = (_loc20.x + _loc19);
+			if (Grid.grid[_loc7] == null)
+			{
+				continue;
+			} // end if
+			for (_loc8 in -1...2)
+			{
+				var _loc5:Int = (_loc20.y + _loc8);
+				if (Grid.grid[_loc7][_loc5] == null)
+				{
+					continue;
+				} // end if
+				for (_loc21 in 0...Grid.grid[_loc7][_loc5].primary.length)
+				{
+					var _loc1:LineBase = Grid.grid[_loc7][_loc5].primary[_loc21];
+					if (_loc1 == null) {
+						continue;
+					}
+					var _loc3:Float = x - _loc1.x1;
+					var _loc2:Float = y - _loc1.y1;
+					var _loc12:Float = Math.sqrt(Math.pow(_loc3, 2) + Math.pow(_loc2, 2));
+					var _loc13:Float = Math.sqrt(Math.pow(x - _loc1.x2, 2) + Math.pow(y - _loc1.y2, 2));
+					var _loc11:Float = Math.abs(_loc1.nx * _loc3 + _loc1.ny * _loc2);
+					var _loc4:Float = (_loc3 * _loc1.dx + _loc2 * _loc1.dy) * _loc1.invSqrDis;
+					if (_loc12 < SVar.eraser_size * _loc9 || _loc13 < SVar.eraser_size * _loc9 || _loc11 < SVar.eraser_size * _loc9 && _loc4 >= 0 && _loc4 <= 1)
+					{
+						return (_loc1);
+					}
+				}
+			}
+		}
+		return null;
 	}
 }
