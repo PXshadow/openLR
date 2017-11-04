2bac6aa0b182e77f6dddc8baec815c0548c80ba3
Kaelan <kbeevans@gmail.com>
2017-11-03 23:49:41 -0600

    Prepwork for better settings box

--- src/lr/settings/SettingsMenu.hx
+++ src/lr/settings/SettingsMenu.hx
@@ -1,5 +1,6 @@
 package lr.settings;
 
+import openfl.display.DisplayObject;
 import openfl.display.Sprite;
 import openfl.events.MouseEvent;
 
@@ -21,83 +22,12 @@ enum PanelMode
  */
 class SettingsMenu extends Sprite
 {
-	var state:PanelMode = file; 
-	  
-	var tabFileMenu:TextButton;
-	var tabTrackSettings:TextButton;
-	var tabProfile:TextButton;
-	var tabEngine:TextButton;
-	
-	var fileMenu:FileMenu;
-	var trackMenu:TrackSettings;
-	var profileMenu:ProfileSettings;
-	var engineMenu:EngineSettings;
-	
-	private var exit:TextButton;
+	var objectList:Array<DisplayObject>;
 	public function new() 
 	{
 		super();
-		
-		this.tabFileMenu = new TextButton(Language.File, this.set_to_file, ButtonSize.b120x30);
-		this.tabTrackSettings = new TextButton(Language.Track, this.set_to_track, ButtonSize.b120x30);
-		this.tabProfile = new TextButton(Language.Profile, this.set_to_profile, ButtonSize.b120x30);
-		this.tabEngine = new TextButton("Global", this.set_to_engine, ButtonSize.b120x30);
-		
-		this.addChild(this.tabEngine);
-		this.tabEngine.x = -140;
-		
-		this.addChild(this.tabFileMenu);
-		this.tabFileMenu.y = 35;
-		this.tabFileMenu.x = -140;
-		
-		this.addChild(this.tabTrackSettings);
-		this.tabTrackSettings.x = -140;
-		this.tabTrackSettings.y = 70;
-		
-		this.exit = new TextButton(Language.Close, Common.gCode.toggleSettings_box, ButtonSize.b120x30);
-		this.addChild(exit);
-		this.exit.x = -140;
-		this.exit.y = 105;
-		
-		this.fileMenu = new FileMenu();
-		this.trackMenu = new TrackSettings();
-		this.engineMenu = new EngineSettings();
-		
-		this.addChild(this.fileMenu);
-		this.addChild(this.trackMenu);
-		this.addChild(this.engineMenu);
-		
-		this.set_to_track();
 	}
 	public function update() {
-		this.fileMenu.update();
-		this.trackMenu.update();
-	}
-	function clearMenu() {
-		this.fileMenu.visible = false;
-		this.trackMenu.visible = false;
-		this.engineMenu.visible = false;
-	}
-	function set_to_engine() 
-	{
-		this.clearMenu();
-		this.state = engine;
-		this.engineMenu.visible = true;
-	}
-	function set_to_track() {
-		this.clearMenu();
-		this.state = track;
-		this.trackMenu.visible = true;
-		this.trackMenu.update();
-	}
-	function set_to_file() {
-		this.clearMenu();
-		this.state = file;
-		this.fileMenu.visible = true;
-	}
-	function set_to_profile() {
-		this.clearMenu();
-		this.state = profile;
-		this.profileMenu.visible = true;
+		
 	}
 }
