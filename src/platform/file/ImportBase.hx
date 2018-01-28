cfb6296b36c7dae68c90ad01f457f703c5e8bdd4
Kaelan <kbeevans@gmail.com>
2018-01-27 23:16:16 -0700

    ImportBase no longer extends from sprite

--- src/platform/file/ImportBase.hx
+++ src/platform/file/ImportBase.hx
@@ -1,17 +1,15 @@
 package platform.file;
 
-import openfl.display.Sprite;
 
 /**
  * ...
  * @author Kaelan Evans
  */
-class ImportBase extends Sprite
+class ImportBase
 {
-
 	public function new() 
 	{
-		super();
+
 	}
 	public function updateList(_path:String, _isRoot:Bool = false) {
 		
