5edb9642e255a8919fc1f4b7734efaa8bf9769d3
Kaelan <kbeevans@gmail.com>
2018-02-11 16:17:26 -0700

    Removed left over experimental code

--- src/lr/scene/Track.hx
+++ src/lr/scene/Track.hx
@@ -153,10 +153,6 @@ class Track extends Sprite
 					this.graphics.lineTo(_line.x2 + (_line.nx * 5 - _line.dx * _line.invDst * 5), _line.y2 + (_line.ny * 5 - _line.dy * _line.invDst * 5));
 					this.graphics.lineTo(_line.x2 - _line.dx * _line.invDst * 5, _line.y2 - _line.dy * _line.invDst * 5);
 					this.graphics.endFill();
-				case LineType.Deccel :
-					this.graphics.lineStyle(2, 0x663300, 1, true, "normal", "round"); 
-					this.graphics.moveTo(_line.x1 + _loc_3, _line.y1 + _loc_4); 
-					this.graphics.lineTo(_line.x2 + _loc_3, _line.y2 + _loc_4);
 				case LineType.Scene: 
 					this.graphics.lineStyle(2, 0x00CC00, 1);
 					this.graphics.moveTo(_line.x1, _line.y1);
