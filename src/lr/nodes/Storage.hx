ddaed649203fe88c8ce0900e23c2dd8cab08ce2f
Kaelan <kbeevans@gmail.com>
2018-04-19 13:42:03 -0600

    Added sorting of lines to ensure accurate physics
    Allows for arbitrary load order, as now when caching, forces lines to be in their correct sorted order as they would in classic and modern builds.

--- src/lr/nodes/Storage.hx
+++ src/lr/nodes/Storage.hx
@@ -21,6 +21,11 @@ class Storage
 		if (_line.type != 2) {
 			this.secondary.push(_line);
 		}
+		haxe.ds.ArraySort.sort(this.secondary, function(a, b):Int {
+			if (a.ID < b.ID) return 1;
+			else if (a.ID > b.ID) return -1;
+			return 0;
+		});
 	}
 	public function remove_line(_line:LineBase) {
 		this.primary.remove(_line);
