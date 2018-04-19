9926bba95e1af3a326de358fbf1bc24bfadcea9d
Kaelan <kbeevans@gmail.com>
2018-04-19 16:16:19 -0600

    Moved sorting algorithm to strictly called when array has item added

--- src/lr/nodes/Storage.hx
+++ src/lr/nodes/Storage.hx
@@ -20,12 +20,12 @@ class Storage
 		this.primary.push(_line);
 		if (_line.type != 2) {
 			this.secondary.push(_line);
+			haxe.ds.ArraySort.sort(this.secondary, function(a, b):Int {
+				if (a.ID < b.ID) return 1;
+				else if (a.ID > b.ID) return -1;
+				return 0;
+			});
 		}
-		haxe.ds.ArraySort.sort(this.secondary, function(a, b):Int {
-			if (a.ID < b.ID) return 1;
-			else if (a.ID > b.ID) return -1;
-			return 0;
-		});
 	}
 	public function remove_line(_line:LineBase) {
 		this.primary.remove(_line);
